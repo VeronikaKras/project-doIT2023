b46c0d110835d6b05e0b7bb8ba1ce7fb04ce2ac8
Veronika <nikakras.dance@gmail.com>
2023-05-24 12:23:08 +0300

    1

---
+++ src/js/new.js
@@ -0,0 +1,27 @@
+const MODAL_ACTIVE_CLASS_NAME = 'active';
+const thankModal = document.querySelector('.modal-thank-you');
+const form = document.querySelector('.thank-form');
+const backdrop = document.querySelector('.overlay');
+const closeBtns = document.querySelectorAll('.close-btn');
+const input = document.querySelector('.feedback-email');
+const openSuccessModal = () => {
+  thankModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+  backdrop.classList.add(MODAL_ACTIVE_CLASS_NAME);
+  console.log('first');
+};
+const closeSuccessModal = () => {
+  thankModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+  backdrop.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+  input.value = '';
+};
+closeBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    e.stopPropagation();
+    closeSuccessModal();
+  });
+});
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  setTimeout(openSuccessModal, 300);
+    setTimeout(closeSuccessModal, 2400);
+});
